b8d6bc6bc50e5f1bf49f0ed13feaef6b839d6817
abc1971 <abc1971@mail.ru>
2020-01-10 18:23:38 +0200

    1

--- functions.php
+++ functions.php
@@ -805,32 +805,32 @@ function cw_woo_attribute(){
         $name = $attribute->get_name();
         if ( $attribute->is_taxonomy() ) {
 
-            // $terms = wp_get_post_terms( $product->get_id(), $name, 'all' );
-            //
-            // $cwtax = $terms[0]->taxonomy;
-            //
-            // $cw_object_taxonomy = get_taxonomy($cwtax);
-            //
-            // if ( isset ($cw_object_taxonomy->labels->singular_name) ) {
-            //     $tax_label = $cw_object_taxonomy->labels->singular_name;
-            // } elseif ( isset( $cw_object_taxonomy->label ) ) {
-            //     $tax_label = $cw_object_taxonomy->label;
-            //     if ( 0 === strpos( $tax_label, 'Product ' ) ) {
-            //         $tax_label = substr( $tax_label, 8 );
-            //     }
-            // }
-            // $display_result .= $tax_label . ': ';
-            // $tax_terms = array();
-            // foreach ( $terms as $term ) {
-            //     $single_term = esc_html( $term->name );
-            //     array_push( $tax_terms, $single_term );
-            // }
-            // $display_result .= implode(', ', $tax_terms) .  '<br />';
+            $terms = wp_get_post_terms( $product->get_id(), $name, 'all' );
+
+            $cwtax = $terms[0]->taxonomy;
+
+            $cw_object_taxonomy = get_taxonomy($cwtax);
+
+            if ( isset ($cw_object_taxonomy->labels->singular_name) ) {
+                $tax_label = $cw_object_taxonomy->labels->singular_name;
+            } elseif ( isset( $cw_object_taxonomy->label ) ) {
+                $tax_label = $cw_object_taxonomy->label;
+                if ( 0 === strpos( $tax_label, 'Product ' ) ) {
+                    $tax_label = substr( $tax_label, 8 );
+                }
+            }
+            $display_result .= $tax_label . ': ';
+            $tax_terms = array();
+            foreach ( $terms as $term ) {
+                $single_term = esc_html( $term->name );
+                array_push( $tax_terms, $single_term );
+            }
+            $display_result .= implode(', ', $tax_terms) .  '<br />';
 
         } else {
             $display_result .= $name . ': ';
             $display_result .= esc_html( implode( ', ', $attribute->get_options() ) ) . '<br />';
-      //      var_dump($attribute);
+
         }
     }
     echo $display_result;
